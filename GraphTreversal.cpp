530dfc6b9b421ce65b37b71d6d9445ab33388d93
Aaron Marklund <aaron.marklund@yahoo.com>
2023-12-09 00:04:43 +0100

    stuff
    Signed-off-by: Aaron Marklund <aaron.marklund@yahoo.com>

--- GraphTreversal.cpp
+++ GraphTreversal.cpp
@@ -1,42 +1,7 @@
 #include "GraphTreversal.h"
-#include <iostream>
-#include <map>
-#include <queue>
-#include <stack>
-using namespace graph_Space;
-// std::shared_ptr<node> Search_Algorithms::Search_Breadth_First(std::vector<std::shared_ptr<node>>&nodelist, int firstid, int goalid)
-// {
-//     if(nodelist.at(firstid)->isblocker || nodelist.at(goalid)->isblocker)
-//     {
-//         std::cerr << "First node or goal is a blocker and can not be found";
-//         return nullptr;
-//     }
-//     std::queue<std::shared_ptr<node>> nodequeue;
-//     nodelist.at(firstid)->explored = true;
-//     nodequeue.push(nodelist.at(firstid));
-//     while(!nodequeue.empty())
-//     {
-//         auto v = nodequeue.front();
-//         nodequeue.pop();
-//         if(v->id == nodelist.at(goalid)->id)
-//         {
-//             std::cout << "Node was found";
-//             return v;
-//         }
-//         for(std::shared_ptr<node>& a : v->adjecent)
-//         {
-//             if(!a->explored)
-//             {
-//                 a->explored = true;
-//                 nodequeue.push(a);
-//             }
-//         }
-//     }
-//     std::cerr << "A path to the goal could not be determined";
-//     return nullptr;
-// }
 
-std::shared_ptr<node> Search_Breadth_First(const std::shared_ptr<node> &first, const std::shared_ptr<node> &goal)
+std::shared_ptr<node> Search_breadth_first
+(std::vector<std::shared_ptr<node>> &nodelist, const std::shared_ptr<node> &first, const std::shared_ptr<node> &goal)
 {
     if(first->isblocker || goal->isblocker)
     {
@@ -44,15 +9,23 @@ std::shared_ptr<node> Search_Breadth_First(const std::shared_ptr<node> &first, c
         return nullptr;
     }
     std::queue<std::shared_ptr<node>> nodequeue;
+
     first->explored = true;
     nodequeue.push(first);
     while(!nodequeue.empty())
     {
-        auto v = nodequeue.front();
+        auto v {nodequeue.front()};
+
         nodequeue.pop();
         if(v->id == goal->id)
+        {
+            for(auto & n : nodelist)
+            {
+                n->explored = false;
+            }
             return v;
-        for(std::shared_ptr<node>& a : v->adjecent)
+        }
+        for(std::shared_ptr<node> & a : v->adjecent)
         {
             if(!a->explored)
             {
@@ -61,31 +34,41 @@ std::shared_ptr<node> Search_Breadth_First(const std::shared_ptr<node> &first, c
             }
         }
     }
-    std::cerr << "A path to the goal could not be determined";
+    std::cerr << "error" <<first->id << " " << goal->id << std::endl;
+    std::cerr << "A path to the goal could not be determined" << std::endl;
     return nullptr;
 }
 
-int Search_Depth_First(const std::shared_ptr<node> &first, const std::shared_ptr<node> &goal)
+void Search_depth_first
+(std::vector<std::shared_ptr<node>> &nodelist, const std::shared_ptr<node> &first, const std::shared_ptr<node> &goal, bool &goalfound)
 {
-    if(first->isblocker)
-    {
-        std::cerr << "Fist node is a blocker and there for can not be used as first node";
-        return 1;
-    }
     first->explored = true;
-    for(auto& a : first->adjecent)
+
+    if(goalfound)
     {
-        if(!a->explored)
-            search_algorithms:Search_Depth_First(a, goal);
+        return;
     }
     if(first->id == goal->id)
     {
-        std::cout << "Goal was found: " << first->id;
-        return 0;
+        for(auto & n : nodelist)
+        {
+            n->explored = false;
+        }
+        goalfound = true;
+        std::cout << "Goal found: " << first->id<< std::endl;
+        return;
     }
-
-    std::cerr << "No goal found";
-    return 1;
+    for(auto & neibor : first->adjecent)
+    {
+        if(!neibor->explored)
+        {
+            Search_depth_first(nodelist, neibor, goal, goalfound);
+        }
+        if(goalfound)
+        {
+            return;
+        }
+    }
 }
 
 double Distance(const vector2 first, const vector2 last)
@@ -106,7 +89,8 @@ std::vector<std::shared_ptr<node>> Reconsturct_path(
     return total_path;
 }
 
-std::vector<std::shared_ptr<node>> Astar_search(const std::shared_ptr<node> &first, const std::shared_ptr<node> &goal)
+std::vector<std::shared_ptr<node>> Astar_search
+(std::vector<std::shared_ptr<node>> &nodelist, const std::shared_ptr<node> &first, const std::shared_ptr<node> &goal)
 {
     std::vector<std::shared_ptr<node>> openset;
     openset.push_back(first);
@@ -120,171 +104,99 @@ std::vector<std::shared_ptr<node>> Astar_search(const std::shared_ptr<node> &fir
     }
 
     std::unordered_map<std::shared_ptr<node>, double> gscore;
-    gscore.reserve(std::numeric_limits<int>::infinity());
-    gscore[first] = 0.0;
+    gscore.reserve(nodelist.size());
 
     std::unordered_map<std::shared_ptr<node>, double> fscore;
-    fscore.reserve(std::numeric_limits<int>::infinity());
-    double d = Distance(first->position, goal->position);
+    fscore.reserve(nodelist.size());
+
+    for(std::shared_ptr<node> & n : nodelist)
+    {
+        gscore[n] = std::numeric_limits<double>::max();
+        fscore[n] = std::numeric_limits<double>::max();
+    }
+
+    gscore[first] = 0.0;
+
+    const double d = Distance(first->position, goal->position);
     fscore[first] = d;
+
     while(!openset.empty())
     {
-        std::shared_ptr<node> current = openset[0];
+        //node with the lowest fscore value
         int j {0};
-        for (int i = 1; i < openset.size(); ++i)
+        std::shared_ptr current {openset[j]};
+        for (int i = 1; i > openset.size(); ++i)
         {
             if(fscore[openset[i]] < fscore[openset[i-1]])
             {
-                current = openset[i];
+                current = openset[i-1];
                 j = i;
             }
         }
-        /* Deprecated code
-        double b {1000};
-        int j {};
-        for (int i = 0; i < openset.size(); i++)
-        {
-            double a = Distance(openset[i]->position, goal->position);
-            if(a < b)
-            {
-                b = a;
-                current = openset[i];
-                j = i;
-            }
-        }
-        */
-         //node with the lowest fscore value
+
         if(current->id == goal->id)
         {
             return Reconsturct_path(camefrom, current);
         }
-        auto some = openset.erase(openset.begin() + j);
-        closedset.push_back(*some);
-        int s{0};
-        for(auto & n :current->adjecent)
+        closedset.push_back(openset[j]);
+        openset.erase(openset.begin() + j);
+        for(auto & n : current->adjecent)
         {
             double t_gscore {gscore[current]};
             t_gscore += Distance(current->position, n->position);
-            if(t_gscore > gscore[n])
+            if(t_gscore < gscore[n])
             {
-                if(std::ranges::find(camefrom.begin(), camefrom.end(), n) == camefrom.end())
-                {
+                /* Does nothing for some reson.
+                 * Should compare n to the existing items in the vector and if they are the same n should not be added to camefrom vector.
+                 * However if I do it correcly no items get added to the vector.
+                 * At the end it != camefrom.end() is the correct way too do it but it does not work for some reson.
+                 */
+                if(auto it = std::ranges::find(camefrom, n); it == camefrom.end())
                     camefrom.push_back(current);
-                }
-
                 gscore[n] = t_gscore;
                 fscore[n] = t_gscore + Distance(n->position, goal->position);
-                if(std::ranges::find(closedset.begin(), closedset.end(), n) == closedset.end())
+                //Also does nothing
+                if(auto it = std::ranges::find(closedset, n); it == closedset.end())
                 {
                     openset.push_back(n);
                 }
             }
         }
     }
+    std::cerr << "Astar goal was not found" << std::endl;
     return openset;
-}
-
-
-/* Could not get depth first to work when useing iterators
- int search_algorithms::searchdepthfirst(std::shared_ptr<node> &first, std::shared_ptr<node> &goal)
- {
-     if(first->isblocker)
-     {
-         std::cerr << "Fist node is a blocker and there for can not be used as first node";
-         return 1;
-     }
-     std::stack<std::shared_ptr<node>> nodestack;
-     first->explored = true;
-     nodestack.push(first);
-     int i {0};
-     int j {0};
-     while (!nodestack.empty())
-     {
-         if(nodestack.top()->adjecent.begin() + i < nodestack.top()->adjecent.end())
-         {
-             auto w = std::next(nodestack.top()->adjecent.begin(), i);
-             if(!w->get()->explored)
-             {
-                 w->get()->explored = true;
-                 nodestack.push(*w);
-             }
-             i++;
-         }
-         else
-         {
-             i = 0;
-             j++;
-             nodestack.pop();
-
-         }
-
-     }
-     std::cerr << "Could not find goal " << j << std::endl;
-     return 1;
-}
-*/
-template<typename T>
-std::vector<double> Mesure_sorting(
-    const int size,
-    std::shared_ptr<node> start[],
-    std::shared_ptr<node> goal[],
-    T (*func) (const std::shared_ptr<node> &f , const std::shared_ptr<node> &l))
-{
-    std::vector<double> duration_list;
-    for (int i = 0; i < size; ++i)
-    {
-        auto s = std::chrono::system_clock::now();
-        func(start[i], goal[i]);
-        auto e = std::chrono::system_clock::now();
-        std::chrono::duration<double> d = e-s;
-        double duration = d.count();
-        duration_list.push_back(duration);
-    }
-    return duration_list;
 }
 
 int main()
 {
-    std::vector<std::shared_ptr<node>> nodelist = graphimport();
-
-    // 0 , 10, 50, 100
-    std::shared_ptr<node> start[]{nodelist[0], nodelist[10], nodelist[50], nodelist[100]};
-
-    //50 , 100, 250, 350
-    std::shared_ptr<node> goal[]{nodelist[50], nodelist[100], nodelist[250], nodelist[350]};
-
-    int size = sizeof(start)/sizeof(std::shared_ptr<node>);
-
-    auto astar = Mesure_sorting(size, start, goal, &Astar_search);
-    auto breadth = Mesure_sorting(size, start, goal, &Search_Breadth_First);
-    auto deapth = Mesure_sorting(size, start, goal, &Search_Depth_First);
+    std::vector<std::shared_ptr<node>> astarlist = graphimport();
+    std::vector<std::shared_ptr<node>> breathlist = graphimport();
 
 
-    for(auto value : astar)
+    int startnodes[] {0, 100, 150, 250};
+    int goalnodes[] {99, 200, 300, 399};
+
+    // Array of all the start nodes
+
+    constexpr int arrsize = sizeof(startnodes)/sizeof(int);
+
+    std::cout << "Astar search: " << std::endl;
+    auto astar = Mesure_sorting(arrsize, astarlist ,startnodes, goalnodes, Astar_search);
+    for(auto const value : astar)
     {
-        std::cout << value << std::endl;
+        std::cout << "Time in seconds: "<< value << std::endl;
     }
-
-    // auto result = Astar_search(nodelist[0], nodelist[10]);
-
-
-    // int result = search_algorithms::searchdepthfirst(nodelist.at(10));
-    //
-    // std::cout << result;
-
-    // search_algorithms::searchbreadthfirst(nodelist, 0, 60);
-
-    // int nodeid = 39;
-    // auto n = nodelist.at(nodeid);
-    // auto pos = n->adjecent;
-    // auto pos1 = nodelist.at(nodeid)->position;
-    // std::cout << "main: " << "x: " << pos1.x << " y: "<< pos1.y << std::endl;
-    // for(const auto& a: pos)
-    // {
-    //
-    //     std::cout << "x: " << a->position.x << " y: " << a->position.y << std::endl;
-    //     std::cout << "Id: " << a->id << std::endl;
-    // }
-
+    std::cout << "Breadth search: " << std::endl;
+    auto breadth = Mesure_sorting(arrsize, breathlist,startnodes, goalnodes, Search_breadth_first);
+    for(auto const value : breadth)
+    {
+        std::cout << "Time in seconds: " << value << std::endl;
+    }
+    std::cout << "Depth search: " << std::endl;
+    auto depth = Mesure_sorting(arrsize, astarlist ,startnodes, goalnodes, Search_depth_first);
+    for(auto const value : astar)
+    {
+        std::cout << "Time in seconds: "<< value << std::endl;
+    }
     return 0;
 }
--- GraphTreversal.h
+++ GraphTreversal.h
@@ -1,9 +1,92 @@
 #ifndef GRAPHTREVERSAL_H
 #define GRAPHTREVERSAL_H
 #include "main.h"
-namespace graph_Space
-{
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
 
+using namespace graph_Space;
+
+template<typename T>
+std::vector<double> Mesure_sorting(
+    const int size,
+    std::vector<std::shared_ptr<node>> &nodelist,
+    int start[],
+    int goal[],
+    T (func) (std::vector<std::shared_ptr<node>> &nlist ,const std::shared_ptr<node> &f , const std::shared_ptr<node> &l))
+{
+    std::vector<double> duration_list;
+    for (int i = 0; i < size; ++i)
+    {
+        auto s = std::chrono::system_clock::now();
+        func(nodelist, nodelist[start[i]], nodelist[goal[i]]);
+        auto e = std::chrono::system_clock::now();
+        std::chrono::duration<double> d = e-s;
+        double duration = d.count();
+        duration_list.push_back(duration);
+    }
+    return duration_list;
 }
+template<typename T>
+std::vector<double> Mesure_sorting(
+    const int size,
+    std::vector<std::shared_ptr<node>> &nodelist,
+    int start[],
+    int goal[],
+    T (func) (std::vector<std::shared_ptr<node>> &nlist ,const std::shared_ptr<node> &f , const std::shared_ptr<node> &l, bool &foundgoal))
+{
+    bool foundg {false};
+    std::vector<double> duration_list;
+    for (int i = 0; i < size; ++i)
+    {
+        auto s = std::chrono::system_clock::now();
+        func(nodelist, nodelist[start[i]], nodelist[goal[i]], foundg);
+        auto e = std::chrono::system_clock::now();
+        std::chrono::duration<double> d = e-s;
+        double duration = d.count();
+        duration_list.push_back(duration);
+    }
+    return duration_list;
+}
+
+class Depth
+{
+    std::shared_ptr<node> Search_depth_first
+(std::vector<std::shared_ptr<node>> &nodelist, const std::shared_ptr<node> &first, const std::shared_ptr<node> &goal)
+    {
+        std::stack<std::shared_ptr<node>> iternodestack;
+        iternodestack.push(first);
+        iternodestack.top()->explored = true;
+        while(!iternodestack.empty())
+        {
+            for(auto & b : iternodestack.top()->adjecent)
+            {
+                for(auto a = b->adjecent.begin(); a < b->adjecent.end(); ++a)
+                {
+                    auto n = *a;
+                    if(n->id == goal->id)
+                    {
+                        for(auto & no : nodelist)
+                        {
+                            n->explored = false;
+                        }
+                        return n;
+                    }
+                    if(!n->explored)
+                    {
+                        n->explored = true;
+                        std::cout << n->id << std::endl;
+                        iternodestack.push(n);
+                    }
+                }
+            }
+            iternodestack.pop();
+        }
+
+        std::cerr << "Depth first could not find: " << goal->id << std::endl;
+        return first;
+    }
+};
 
 #endif //GRAPHTREVERSAL_H
